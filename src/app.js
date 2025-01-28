e0a9e1c0da7e6cdf65ad52f5c8a3068e2ca1090c
bbantier <65014410+bbantier@users.noreply.github.com>
2025-01-28 16:15:45 +0100

    create new project button and style

--- src/app.js
+++ src/app.js
@@ -7,21 +7,3 @@ import { Project, ProjectForm } from "./project";
 
 const mainDiv = document.querySelector(".main");
 const sidebar = document.querySelector(".sidebar");
-
-// const todoForm = new TodoForm(); // remove
-// mainDiv.appendChild(todoForm.render()); //remove
-
-const todoItem = new TodoItem("This is a todo item", "", "2025-01-28", "High"); // remove
-mainDiv.appendChild(todoItem.render()); // remove
-
-const todoItemTwo = new TodoItem("This is a todo item", "", "2025-01-28", "High"); // remove
-mainDiv.appendChild(todoItemTwo.render()); // remove
-
-
-const projects = document.createElement("ul");
-const projectForm = new ProjectForm();
-
-projects.className = "project-list";
-projects.appendChild(projectForm.render());
-
-sidebar.appendChild(projects);
