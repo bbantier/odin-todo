7bef4baa8db32442e1bd3933122b032fb8d3db34
bbantier <65014410+bbantier@users.noreply.github.com>
2025-01-28 17:06:34 +0100

    experimenting with localStorage in project.js

--- src/project.js
+++ src/project.js
@@ -1,12 +1,13 @@
 export class Project {
   constructor(name) {
     this.name = name;
+    this.todos = [];
   }
 
   render() {
     const projectItem = document.createElement("li");
-
     projectItem.className = "project-item";
+    projectItem.textContent = this.name;
 
     return projectItem;
   }
@@ -26,6 +27,27 @@ export class ProjectForm {
 
     projectForm.appendChild(projectInput);
 
+    projectForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const projectInput = document.querySelector(".project-input");
+      const projectItem = new Project(projectInput.value);
+
+      localStorage.setItem(projectItem.name, projectItem.todos);
+      refreshProjectList();
+    });
+
     return projectForm;
   }
 }
+
+const refreshProjectList = () => {
+  const projectList = document.querySelector(".project-list");
+  const children = document.querySelectorAll(".project-item");
+
+  children.forEach((element) => element.remove());
+
+  Object.keys(localStorage).forEach((key) => {
+    projectList.appendChild(new Project(key).render());
+  })
+};
