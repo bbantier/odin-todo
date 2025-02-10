09650b3c83f0057f34c9dfc20db2f4cfc6e17a70
bbantier <65014410+bbantier@users.noreply.github.com>
2025-02-10 10:23:39 +0100

    resetting project

--- src/app.js
+++ src/app.js
@@ -2,32 +2,11 @@ import "./style.css";
 import "./todo-form.css";
 import "./todo-item.css";
 import "./project.css";
-import { TodoItem, TodoForm } from "./todo";
-import { Project, ProjectList, ProjectForm } from "./project";
 
 const main = document.querySelector(".main");
 const sidebar = document.querySelector(".sidebar");
 const newProjectButton = document.querySelector(".new-project");
 
-document.addEventListener("DOMContentLoaded", () => refreshUi());
-
-newProjectButton.addEventListener("click", () => {
-  const projectList = document.querySelector(".project-list");
-  const projectForm = new ProjectForm().render();
-
-  projectList.appendChild(projectForm);
-
-  const projectInput = document.querySelector(".project-input");
-  projectInput.focus();
-});
-
-
-export const refreshUi = () => {
-  const oldList = document.querySelector(".project-list");
-  if(oldList) oldList.remove();
-  const projectList = new ProjectList();
-
-  projectList.refresh();
-  projectList.init();
-  sidebar.insertBefore(projectList.render(), newProjectButton);
-}
+const projectList = (() => {
+  const list = [];
+})();
