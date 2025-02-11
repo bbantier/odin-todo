565ae91aef405eb00346b012a6249e8714a0c287
bbantier <65014410+bbantier@users.noreply.github.com>
2025-02-11 10:34:40 +0100

    create todo render method

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,6 @@
 const createTodo = (title, description, dueDate, priority) => {
   const id = crypto.randomUUID();
+  let done = false;
 
   return { id, title, description, dueDate, priority };
 }
@@ -7,5 +8,19 @@ const createTodo = (title, description, dueDate, priority) => {
 export default function addTodo(title, description, dueDate, priority) {
   const todo = createTodo(title, description, dueDate, priority);
 
-  return Object.assign({}, todo);
+  const render = (container) => {
+    const todoItem = document.createElement("div");
+
+    todoItem.className = "todo-item";
+    todoItem.innerHTML = `
+      <div class="todo-item-left">
+        <input type="checkbox">
+        <h2 class="todo-title">${title}</h2>
+      </div>
+      <div class="todo-date">${dueDate}</div>
+    `
+
+    container.appendChild(todoItem);
+  }
+  return Object.assign({}, todo, { render });
 }
